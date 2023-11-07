b2c9c708ca05ec819e32370d29790f5a91f07cde
Goldilocks <ceo5584@gmail.com>
2023-11-07 16:33:35 +0900

    add cpp class, create game mode gm_8

---
+++ Source/NCA_KDW/NCA_KDW.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NCA_KDW.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NCA_KDW, "NCA_KDW" );
---
+++ Source/NCA_KDW/gm_8.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gm_8.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NCA_KDW_API Agm_8 : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
